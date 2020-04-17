7d2dae80f943b21a0ede7a0a71ddd488a4e6d919
Amel <zekafrombtm@hotmail.com>
2020-04-17 20:49:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () =>{
+
+
+  const [state, setState] = useState([]);
+  const [quote,setQuote] = useState({})
+
+  useEffect(() => {
+fetch("https://type.fit/api/quotes")
+.then(response => response.json())
+.then(data => {
+  setState(data);
+  setQuote(data[Math.floor(Math.random() * state.length)]);
+})
+.catch(err => {
+	console.log(err);
+});
+  },[]);
+
+  const randomQuote = () => {
+    setQuote(state[Math.floor(Math.random() * state.length)]);
+    console.log(quote.text);
+    console.log("1");
+  };
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Random Quote Machine</h1>
+
+        <div id="quote-box">
+          <div className="box">
+            
+              <div className="quote">  
+                  <div id="text">"{quote.text}"</div>
+                  {     quote.author
+                         ?
+                          <div id="author">-{quote.author}</div>
+                        :
+                           <div id="author">-Unknown</div> 
+                  }
+              </div>
+            
+              <div className="buttons">
+              <a class="twitter-share-button"
+              title="Tweet this quote!"
+              target="_blank"
+              href={`https://twitter.com/intent/tweet?text="${quote.text} ${quote.author}"`}
+              data-size="large"><i class="fab fa-twitter-square fa-2x icon_a"></i></a>
+                <button onClick={randomQuote} className="newquote">New Quote</button>
+                
+              </div>
+          </div>
+
+
+        </div>
+
+
     </div>
   );
 }
