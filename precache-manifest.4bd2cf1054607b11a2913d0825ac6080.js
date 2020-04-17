f9045119e99e8e68fa71c450bfe516e6caea7124
Amel <zekafrombtm@hotmail.com>
2020-04-17 20:52:17 +0200

    Updates

---
+++ precache-manifest.4bd2cf1054607b11a2913d0825ac6080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f093b5f00df533690d32ce4953c0ad5c",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "5acc09a94f5457d5fe12",
+    "url": "/RandomQuoteMachine/static/css/main.a4e37db9.chunk.css"
+  },
+  {
+    "revision": "b87cd637440e7bfa1071",
+    "url": "/RandomQuoteMachine/static/js/2.3a7bc801.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RandomQuoteMachine/static/js/2.3a7bc801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5acc09a94f5457d5fe12",
+    "url": "/RandomQuoteMachine/static/js/main.0860418e.chunk.js"
+  },
+  {
+    "revision": "c33a58bf105c8fa00e1f",
+    "url": "/RandomQuoteMachine/static/js/runtime-main.a62257a5.js"
+  }
+]);
